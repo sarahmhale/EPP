2aed9562c5ca035480244d0f98b262e15af5e734
Sarah <sarahhale95@gmail.com>
2018-11-01 18:26:47 +0100

    lama

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -5,6 +5,7 @@ import {
   StyleSheet,
   Image
 } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
 export default class SettingsScreen extends React.Component {
   static navigationOptions = {
@@ -12,12 +13,11 @@ export default class SettingsScreen extends React.Component {
   };
 
   render() {
-    /* Go ahead and delete ExpoConfigView and replace it with your
-     * content, we just wanted to give you a quick view of your config */
     return (
       <View style={styles.container}>
       <Image style={styles.image} source={{uri: 'https://i.pinimg.com/originals/f8/12/8c/f8128c045ac5bda679971ab7bacace1e.jpg'}}/>
         <Text>Namn Namnsson</Text>
+        <Text>Prestationer</Text> 
       </View>);
   }
 }
