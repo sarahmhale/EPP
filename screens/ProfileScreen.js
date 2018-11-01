ca0df996195dc083afd77e9e0cca061103018a39
Sarah <sarahhale95@gmail.com>
2018-11-01 18:20:16 +0100

    circle image

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -3,6 +3,7 @@ import {
   View,
   Text,
   StyleSheet,
+  Image
 } from 'react-native';
 
 export default class SettingsScreen extends React.Component {
@@ -15,15 +16,23 @@ export default class SettingsScreen extends React.Component {
      * content, we just wanted to give you a quick view of your config */
     return (
       <View style={styles.container}>
-        <Text>Profile</Text>
+      <Image style={styles.image} source={{uri: 'https://i.pinimg.com/originals/f8/12/8c/f8128c045ac5bda679971ab7bacace1e.jpg'}}/>
+        <Text>Namn Namnsson</Text>
       </View>);
   }
 }
 
 const styles = StyleSheet.create({
   container: {
+    alignItems: 'center',
     flex: 1,
     paddingTop: 15,
     backgroundColor: '#fff',
   },
+  image: {
+    margin:30,
+    height: 100,
+    borderRadius: 50,
+    width: 100
+  }
 });
